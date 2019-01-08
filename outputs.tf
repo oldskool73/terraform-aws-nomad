d50a94b006f40467b91ed9d857ced22e0962c54b
Adam Robertson <adam.robertson@imagination.com>
2019-01-09 00:43:19 +1100

    remove client creation

--- outputs.tf
+++ outputs.tf
@@ -22,29 +22,7 @@ output "security_group_id_servers" {
   value = "${module.servers.security_group_id}"
 }
 
-output "num_clients" {
-  value = "${module.clients.cluster_size}"
-}
 
-output "asg_name_clients" {
-  value = "${module.clients.asg_name}"
-}
-
-output "launch_config_name_clients" {
-  value = "${module.clients.launch_config_name}"
-}
-
-output "iam_role_arn_clients" {
-  value = "${module.clients.iam_role_arn}"
-}
-
-output "iam_role_id_clients" {
-  value = "${module.clients.iam_role_id}"
-}
-
-output "security_group_id_clients" {
-  value = "${module.clients.security_group_id}"
-}
 
 output "aws_region" {
   value = "${data.aws_region.current.name}"
